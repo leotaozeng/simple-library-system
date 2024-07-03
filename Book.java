4196d3b7a59a593a727ab4989e5ef416c9c8864e
Leo Zeng <qingchun1995@gmail.com>
2024-07-03 21:17:58 +0800

    Change the Book class attributes to private

--- Book.java
+++ Book.java
@@ -1,7 +1,7 @@
 public class Book {
-    String title;
-    String author;
-    int quantity;
+    private String title;
+    private String author;
+    private int quantity;
 
     // Create a class constructor for the Book class
     public Book(String title, String author, int quantity) {
@@ -10,10 +10,20 @@ public class Book {
         this.quantity = quantity;
     }
 
+    // Getter
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     public int getQuantity() {
         return quantity;
     }
 
+    // Setter
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
